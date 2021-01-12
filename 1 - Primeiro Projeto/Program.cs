647d3c3a872936c3e370a9cfc5dccd35b9c4162f
ygors <ygors@DESKTOP-746LT0F>
2021-01-12 17:43:52 -0300

    Alterações novas

---
+++ 1 - Primeiro Projeto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1___Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo. Projeto no Visual Studio");
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
